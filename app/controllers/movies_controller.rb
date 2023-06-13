9609986ff49ce3ddb3106f357f7b257533970c9a
teacuppigbee <jesslynwong22@gmail.com>
2023-06-13 22:42:28 +0000

    done

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,5 +1,5 @@
 class MoviesController < ApplicationController
-
+  before_action :check_home_page, only: [:index]
   def show
     id = params[:id] # retrieve movie ID from URI route
     @movie = Movie.find(id) # look up movie by unique ID
@@ -14,35 +14,37 @@ class MoviesController < ApplicationController
 
   def index
     @all_ratings = Movie.all_ratings
-
-    @selected_ratings = params[:ratings] || session[:ratings] || []
-
-    # Fetch movies based on selected ratings
-    @movies = Movie.with_ratings(@selected_ratings)
-
-    # Create a hash to track which ratings should be checked
-    @ratings_to_show_hash = Hash[@selected_ratings.map { |rating| [rating, true] }]
-
-    @selected_column = params[:sort] || session[:sort] # Store the selected column in an instance variable
-
-    case @selected_column
-    when 'title'
-      @movies = @movies.order(title: :asc)
-    when 'release_date'
-      @movies = @movies.order(release_date: :asc)
+    
+    if session[:home] == 0 && !params[:ratings]
+      @selected_ratings = params[:ratings]
+      @selected_column = params[:sort]
+      session[:sort] = params[:sort]
+      @movies = Movie.with_ratings(@selected_ratings)
+      @ratings_to_show_hash = Hash[@all_ratings.map { |rating| [rating, false] }]
+    else
+      @selected_ratings = params[:ratings] || session[:ratings] || {}
+      @selected_column = params[:sort] || session[:sort] # Store the selected column in an instance variable
+      @movies = Movie.with_ratings(@selected_ratings.keys)
+      @ratings_to_show_hash = Hash[@all_ratings.map { |rating| [rating, @selected_ratings.key?(rating)] }]
+    
+      case @selected_column
+      when 'title'
+        @movies = @movies.order(title: :asc)
+      when 'release_date'
+        @movies = @movies.order(release_date: :asc)
+      end
+    
+      if params[:ratings].blank? && session[:ratings].present? && session[:ratings] != {}
+        redirect_to movies_path(ratings: session[:ratings], sort: @selected_column)
+        return
+      end
     end
-
-    session[:sort] = @selected_column if @selected_column.present?
-    session[:ratings] = @selected_ratings if @selected_ratings.present?
-
-    if params[:sort].blank? && params[:ratings].blank?
-      @selected_column = session[:sort]
-      @selected_ratings = session[:ratings]
-      @selected_ratings = @selected_ratings if @selected_ratings.present?
-    end
-  end
-
   
+    session[:ratings] = @selected_ratings if params[:ratings].present?
+    session[:sort] = @selected_column if params[:sort].present?
+  end
+  
+
 
   def new
     # default: render 'new' template
@@ -78,5 +80,11 @@ class MoviesController < ApplicationController
   def movie_params
     params.require(:movie).permit(:title, :rating, :description, :release_date)
   end
+
+  def check_home_page
+    session[:home] = 0 
+    session[:home] = 1 if request.referrer.present? && request.referrer.include?("/movies/")
+  end
+
 
 end
